796f3000ccc9dba93786d79ff479de7154c7b486
Lex <124043026+esistlex@users.noreply.github.com>
2023-09-13 17:24:38 +0800

    Add files via upload

---
+++ Do - While - Meralco/ConsoleApp1/do-while-while.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grades = 0, score = 0, average = 0;
+            int counter, limit, items = 0;
+            char answer = '\0';
+
+            do
+            {
+                Console.Clear();
+                counter = 1;
+                average = 0;
+
+                Console.Write("How many times to loop? ");
+                limit = Convert.ToInt32(Console.ReadLine());
+
+                while (counter <= limit)
+                {
+                    Console.Write("[" + counter + "]" + "Enter raw score: ");
+                    score = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("[" + counter + "]" + "Enter raw items: ");
+                    items = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    grades = grades + ConvertScore(score, items);
+                    counter++;
+                }
+                Console.WriteLine("\nTotal grade is: " + (grades / limit));
+                Console.WriteLine("Do you want to continue? Enter (Y) to continue.");
+                answer = Convert.ToChar(Console.ReadLine());
+            }
+            while (answer == 'Y' || answer == 'y');
+            Console.ReadLine();
+        }
+
+        public static double ConvertScore(double rawscore, int items)
+        {
+            double grade;
+            grade = (rawscore / items) * 50 + 50;
+            return grade;
+        }
+    }
+}
---
+++ Do - While - Meralco/ConsoleApp1/do-while.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grades = 0, score = 0, average = 0;
+            int items, counter;
+            char answer = '\0';
+            do
+            {
+                Console.Clear();
+                for (counter = 1; counter <= 5; counter++)
+                {
+                    Console.Write("[" + counter + "]" + "Enter raw score: ");
+                    score = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("[" + counter + "]" + "Enter raw items: ");
+                    items = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    grades = grades + ConvertScore(score, items);
+                    counter++;
+                }
+                Console.WriteLine("Total grade is: " + (grades / 5));
+                Console.WriteLine("Do you want to continue? Enter (Y) to continue.");
+                answer = Convert.ToChar(Console.ReadLine());
+            } while (answer == 'Y' || answer == 'y');
+            Console.ReadKey();
+        }
+
+        public static double ConvertScore(double rawscore, int items)
+        {
+            double grade;
+            grade = (rawscore / items) * 50 + 50;
+            return grade;
+        }
+    }
+}
